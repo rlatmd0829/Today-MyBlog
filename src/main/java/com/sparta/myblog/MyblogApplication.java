197653c303d4b8bf2f0dbc1dfff84b08c642c7e3
seungwook <rlatmd0829@naver.com>
2021-03-24 14:12:43 +0900

    html 수정

--- src/main/java/com/sparta/myblog/MyblogApplication.java
+++ src/main/java/com/sparta/myblog/MyblogApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.sql.Time;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class MyblogApplication {
@@ -12,4 +16,9 @@ public class MyblogApplication {
         SpringApplication.run(MyblogApplication.class, args);
     }
 
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+    }
 }
